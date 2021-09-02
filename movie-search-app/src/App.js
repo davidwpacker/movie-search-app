7fa212766668bed3ed940d65f323311c3eaa7bf0
davidwpacker <davidwgpacker@gmail.com>
2021-09-02 17:20:29 +1000

    state added to component to track user's query and when the movies are returned

---
+++ movie-search-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchMovie from './SearchMovie'
+
+function App() {
+  return (
+    <div className="container">
+        <h1 className="title"> React Movie Search </h1>
+        <SearchMovie/>
+
+        <p> hellooo </p>
+    </div>
+    
+  );
+}
+
+export default App;
