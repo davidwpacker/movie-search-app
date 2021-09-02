42a718111147e17eee19ede1746cc828b72b96c4
davidwpacker <davidwgpacker@gmail.com>
2021-09-02 18:55:56 +1000

    Movie card component

--- movie-search-app/src/App.js
+++ movie-search-app/src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import SearchMovie from './SearchMovie'
+import SearchMovie from './searchMovie'
 
 function App() {
   return (
     <div className="container">
         <h1 className="title"> React Movie Search </h1>
         <SearchMovie/>
-
-        <p> hellooo </p>
     </div>
     
   );
